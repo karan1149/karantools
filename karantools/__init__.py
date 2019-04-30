0de90e96447c0bda99217f459c2f783fbcabd469
Karan Singhal <karan1149@gmail.com>
2019-04-30 14:58:09 -0700

    Initial commit with assert, average, and color functions

---
+++ karantools/__init__.py
@@ -0,0 +1,80 @@
+from collections import defaultdict
+
+def average(arr):
+    return float(sum(arr)) / len(arr)
+
+def average_or_0(arr):
+    if len(arr):
+        return float(sum(arr)) / len(arr)
+    else:
+        return 0
+
+class AverageStreamer(object):
+    def __init__(self):
+        self.count = 0
+        self.total = 0.0
+
+    def add(self, x):
+        self.total += x
+        self.count += 1
+
+    def query(self):
+        return self.total / self.count
+
+def print_comment_header_block(header_string, length=70):
+    print('#' * length)
+    left_spaces = (length - 2 - len(header_string)) // 2
+    right_spaces = length - 2 - len(header_string) - left_spaces
+    print('#' + ' ' * left_spaces + header_string + ' ' * right_spaces + '#')
+    print('#' * length)
+
+######################################################################
+#                              ASSERTS                               #
+######################################################################
+
+def assert_eq(a, b):
+    if a != b:
+        print_bold("Assert failed because the following two are unequal:")
+        print(a)
+        print(b)
+        raise AssertionError()
+
+def assert_float_eq(a, b, epsilon=1e-5):
+    if abs(a - b) > epsilon:
+        print_bold("Assert failed because the following two are unequal:")
+        print(a)
+        print(b)
+        raise AssertionError()
+
+def assert_neq(a, b):
+    if a == b:
+        print_bold("Assert failed because the following two are equal:")
+        print(a)
+        print(b)
+        raise AssertionError()
+
+def assert_float_neq(a, b, epsilon=1e-5):
+    if abs(a - b) <= epsilon:
+        print_bold("Assert failed because the following two are approximately equal:")
+        print(a)
+        print(b)
+        raise AssertionError()
+
+######################################################################
+#                               COLORS                               #
+######################################################################
+
+class color:
+   PURPLE = '\033[95m'
+   CYAN = '\033[96m'
+   DARKCYAN = '\033[36m'
+   BLUE = '\033[94m'
+   GREEN = '\033[92m'
+   YELLOW = '\033[93m'
+   RED = '\033[91m'
+   BOLD = '\033[1m'
+   UNDERLINE = '\033[4m'
+   END = '\033[0m'
+
+def print_bold(string):
+    print(color.BOLD + string + color.END)
